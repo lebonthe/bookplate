d2b1ddbcfdffff1fd2a4060f55d5ff6ab60aff1d
Min Hu <lebonthe@gmail.com>
2023-09-12 23:01:53 +0800

    Initial Commit

---
+++ testCanDeleteNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testCanDeleteNow
+//
+//  Created by Min Hu on 2023/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
